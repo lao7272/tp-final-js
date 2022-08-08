7d35e838fc349e32beba3db3689c51d11554c36d
Lao <estanislaolanus@gmail.com>
2022-08-08 11:42:49 -0300

    first commit

---
+++ scripts/Carrito.js
@@ -0,0 +1,15 @@
+const buttonCart =  document.getElementById('buttonCart');
+
+buttonCart.addEventListener('click', () => {
+    
+
+Swal.fire({
+    title: '<strong><h3>Carrito de compras </h3></strong>',
+    html:
+        `<b><p></p></b>`,
+    showCloseButton: true,
+    showCancelButton: true,
+    focusConfirm: false,
+    
+    });
+});
